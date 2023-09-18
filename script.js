c5179cbe9afae2ccdbde49d0b8f7e511b46732ad
Steven Hoang <Stevendqhoang@Gmail.com>
2023-09-18 14:16:24 -0700

    clear all

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const DEFAULT_COLOR = '#333333'
 const DEFAULT_MODE = 'color'
 const DEFAULT_SIZE = 16
 
+<<<<<<< HEAD
 let currentColor = DEFAULT_COLOR
 let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
+=======
+const numRows    = 6
+const numColumns = 6
+>>>>>>> parent of 808bb83 (set some flexbox properties)
 
 function setCurrentColor(newColor) {
   currentColor = newColor
